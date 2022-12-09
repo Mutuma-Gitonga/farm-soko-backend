752bc35112e219c7bc973b96627255640179ed2f
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-09 19:46:28 +0300

    feat: add all model association macros

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -5,4 +5,8 @@ class Consumer < ApplicationRecord
   validates :town, length: {minimum: 3}
   validates :phone, length: {minimum: 12}
   validates :username, length: {minimum: 5}, presence: true, uniqueness: true
+
+  has_many :sale_purchases
+  has_many :produces, through: :sale_purchases
+  has_many :farmers, through: :produces 
 end
