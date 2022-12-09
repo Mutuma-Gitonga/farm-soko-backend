4a0583a255be906cc909114988678609b3aaeb23
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-09 19:44:44 +0300

    feat: add all model association macros

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,11 +1,13 @@
 class Farmer < ApplicationRecord
   has_secure_password
-
-  has_many :produces
-
   validates :name, :town, :phone, presence: true
   validates :name, length: {minimum: 5}
   validates :town, length: {minimum: 3}
   validates :phone, length: {minimum: 12}
   validates :username, length: {minimum: 5}, presence: true, uniqueness: true
+
+  has_many :produces
+  has_many :sale_purchases, through: :produces
+  has_many :consumers, through: :sale_purchases
+  
 end
