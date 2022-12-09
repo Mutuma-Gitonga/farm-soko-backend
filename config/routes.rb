bd07de53a79e5b4f79c9094d75a2f2f34e602079
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-09 14:12:22 +0300

    feat: add farmer signup and autologin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/farmer_signup", to: "farmers#create"
+  get "/farmer", to: "farmers#show"
 end
