253c7b612622a9973e6eba2215d31ad4067b0da9
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-09 14:23:19 +0300

    feat: add farmer session login & logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # Farmer signup & autologin routes
   post "/farmer_signup", to: "farmers#create"
   get "/farmer", to: "farmers#show"
+
+  # Farmer login & logout routes
+  post "/farmer_login", to: "farmer_sessions#create"
+  delete "/farmer_logout", to: "farmer_sessions#destroy"
 end
