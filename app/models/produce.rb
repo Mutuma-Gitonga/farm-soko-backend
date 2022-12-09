d510b9546233b5606d1183f9ee1c10edcf73c0bb
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-09 19:45:43 +0300

    feat: add model association macros

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -1,6 +1,10 @@
 class Produce < ApplicationRecord
-  belongs_to :farmer 
-  
+
   validates :name, :quantity, :units, :unit_price, presence: true
+
+  belongs_to :farmer 
+  has_one :sale_purchase, foreign_key: :produce_id
+  # OR SIMPLY:
+  # has_one :sale_purchase
   
 end
