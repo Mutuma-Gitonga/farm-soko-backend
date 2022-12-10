548503ad4098b23e7bf4401ebbc500c712e9e847
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 18:51:42 +0300

    fix: alter association macros

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -3,8 +3,10 @@ class Produce < ApplicationRecord
   validates :name, :quantity, :units, :unit_price, presence: true
 
   belongs_to :farmer 
-  has_one :sale_purchase, foreign_key: :produce_id
-  belongs_to :consumer, through: :sale_purchase
+  # has_one :sale_purchase, foreign_key: :produce_id
+  has_one :sale_purchase
+  # belongs_to :consumer, through: :sale_purchase
+  
   # OR SIMPLY:
   # has_one :sale_purchase
   
