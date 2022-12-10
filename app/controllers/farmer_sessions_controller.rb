91ea6be19f03ecb20aa2c638aa46232e2eafdb25
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 18:53:27 +0300

    fix: edit login error message format

--- app/controllers/farmer_sessions_controller.rb
+++ app/controllers/farmer_sessions_controller.rb
@@ -1,12 +1,14 @@
+# require 'pry'
 class FarmerSessionsController < ApplicationController
   def create
     farmer = Farmer.find_by(username: params[:username])
+    # binding.pry
     
     if farmer&.authenticate(params[:password])
       session[:farmer_id] = farmer.id
       render json: farmer, status: :created
     else
-      render json: { error: "Invalid username or password" }, status: :unauthorized
+      render json: { errors: ["Invalid username or password"]}, status: :unauthorized
     end
   end
 
