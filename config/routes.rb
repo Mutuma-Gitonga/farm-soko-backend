10d437110ce7591f58aa777a9a62402c79f0918c
Mutuma Gitonga <mgitonga150@gmail.com>
2022-12-10 18:57:25 +0300

    fix: change authentication/authorization/produce/farmers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
   # Farmer signup & autologin routes
-  post "/farmer_signup", to: "farmers#create"
-  get "/farmer", to: "farmers#show"
+  # post "/farmer_signup", to: "farmers#create"
+  # get "/farmer", to: "farmers#show"
+  post "/signup", to: "farmers#create"
+  get "/me", to: "farmers#show"
 
   # Farmer login & logout routes
-  post "/farmer_login", to: "farmer_sessions#create"
-  delete "/farmer_logout", to: "farmer_sessions#destroy"
+  # post "/farmer_login", to: "farmer_sessions#create"
+  # delete "/farmer_logout", to: "farmer_sessions#destroy"
+  post "/login", to: "farmer_sessions#create"
+  # match "/login", to: "farmer_sessions#create", via: [:options, :post]
 
-   # Consumer signup & autologin routes
-   post "/consumer_signup", to: "consumers#create"
-   get "/consumer", to: "consumers#show"
+  delete "/logout", to: "farmer_sessions#destroy"
 
-   # Consumer login & logout routes
-  post "/consumer_login", to: "consumer_sessions#create"
-  delete "/consumer_logout", to: "consumer_sessions#destroy"
+  #  # Consumer signup & autologin routes
+  #  post "/consumer_signup", to: "consumers#create"
+  #  get "/consumer", to: "consumers#show"
+
+  #  # Consumer login & logout routes
+  # post "/consumer_login", to: "consumer_sessions#create"
+  # delete "/consumer_logout", to: "consumer_sessions#destroy"
 
 
   # PRODUCE CRUD OPERATIONS
@@ -27,5 +33,10 @@ Rails.application.routes.draw do
   patch "/produce/:id", to: "produces#update"
   # Farmer destroys a product 
   delete "/produce/:id", to: "produces#destroy"
+
+  # GET FARMERS
+  get "/farmers", to: "farmer_extras#index"
+  # GET FARMER 
+  get "/farmers/:id", to: "farmer_extras#show"
 
 end
